f76143ce918348ce3a1b816ead80c7b136a4eec1
veronikashishlo <veronika.shyshlo@gmail.com>
2025-05-30 12:18:48 +0300

    Добавлены тесты на проверку ошибок при авторизации

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -4,16 +4,43 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators:
     LOGIN_TAB = (By.XPATH, '//*[@data-l="t,login_tab"]')
     QR_TAB = (By.XPATH, '//*[@data-l="t,qr_tab"]')
-    LOGIN_FIELD = (By.ID, 'field_emai')
-    PASSWORD_FIELD = (By.ID, 'field_password"')
+    LOGIN_FIELD = (By.ID, 'field_email')
+    PASSWORD_FIELD = (By.ID, 'field_password')
     LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,sign_in"]')
     LOGIN_BY_QR_CODE = (By.XPATH, '//*[@data-l="t,get_qr"]')
     RECOVERY_LINK = (By.XPATH, '//*[@data-l="t,restore"]')
-    REGISTRATION_BUTTON = (By.XPATH, '//div[@class="external-oauth-login-footer"]//*[@data-l="t,register"]')
+    REGISTRATION_BUTTON = (By.XPATH, '//div[@class="external-oauth-login-footer"]/a[@data-l="t,register"]')
     VK_BUTTON = (By.XPATH, '//*[@data-l="t,vkc"]')
     MAIL_BUTTON = (By.XPATH, '//*[@data-l="t,mailru"]')
     YANDEX_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
+    ERROR_TEXT = (By.XPATH, '//*[@class="input-e login_error"]')
 
 
 class LoginPageHelper(BasePage):
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.LOGIN_TAB)
+        self.find_element(LoginPageLocators.QR_TAB)
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_BY_QR_CODE)
+        self.find_element(LoginPageLocators.RECOVERY_LINK)
+        self.find_element(LoginPageLocators.REGISTRATION_BUTTON)
+        self.find_element(LoginPageLocators.VK_BUTTON)
+        self.find_element(LoginPageLocators.MAIL_BUTTON)
+        self.find_element(LoginPageLocators.YANDEX_BUTTON)
+
+    def click_login(self):
+        self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
+
+    def get_error_text(self):
+        return self.find_element(LoginPageLocators.ERROR_TEXT).text
+
+    def set_login(self, login):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(login)
+
+
