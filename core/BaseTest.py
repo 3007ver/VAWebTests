dde7f6c44ddd10e972635c6eb1b3d76383a2191b
veronikashishlo <veronika.shyshlo@gmail.com>
2026-07-29 14:33:04 +0300

    Добавлены файлы requirements, pytest.ini и readme

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -1,9 +1,16 @@
+import os
 import pytest
 from selenium import webdriver
 
+
 @pytest.fixture(scope='session')
 def browser():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    if os.getenv("HEADLESS", "false").lower() == "true":
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
